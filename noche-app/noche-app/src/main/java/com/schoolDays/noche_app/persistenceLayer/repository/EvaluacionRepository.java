3e2ce5a85e717e96854954659c21c89a428882aa
jorge luis <martinezmonajorgeluis@gmail.com>
2025-09-17 07:21:22 -0500

    se borran los repositorios anteriores

--- noche-app/noche-app/src/main/java/com/schoolDays/noche_app/persistenceLayer/repository/EvaluacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.schoolDays.noche_app.persistenceLayer.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluacionRepository extends CrudRepository<EvaluacionEntity, Long> {
-
-}
--- noche-app/noche-app/src/main/java/com/schoolDays/noche_app/persistenceLayer/repository/RespuestaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.schoolDays.noche_app.persistenceLayer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RespuestaRepository extends JpaRepository<RespuestaEntity, Long> {
-}
